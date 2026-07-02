34e48936063fe381140177052e46597c937408a7
kimbyungnam <kimbyungnam@ajou.ac.kr>
2026-07-02 13:03:33 +0900

    [test] 현재날짜가_일요일인_경우_예약불가_예외처리

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -128,6 +128,11 @@ def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_wit
 
 
 def test_현재날짜가_일요일인_경우_예약불가_예외처리():
+    booking_scheduler = SundayBookingScheduler(CAPACITY_PER_HOUR)
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+
+    with pytest.raises(ValueError):
+        booking_scheduler.add_schedule(schedule)
     pass
 
 
