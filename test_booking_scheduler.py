95002abc876888d06013d264b11878c8be68c2c1
kimbyungnam <kimbyungnam@ajou.ac.kr>
2026-07-02 12:39:09 +0900

    [test] 예약은_정시에만_가능하다_정시가_아닌경우_예약불가

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -1,4 +1,5 @@
 import pytest
+from datetime import datetime
 
 from schedule import Customer, Schedule
 from communication import SmsSender, MailSender
@@ -6,12 +7,19 @@ from booking_scheduler import BookingScheduler
 
 
 def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가():
+    # arrange
+    not_on_the_hour = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
+    customer = Customer("Fake name", "010-1234-5678")
+    schedule = Schedule(not_on_the_hour, 1, customer)
+    booking_scheduler = BookingScheduler(3)
+    # act and assert
+    with pytest.raises(ValueError):
+        booking_scheduler.add_schedule(schedule)
     pass
 
 
 def test_예약은_정시에만_가능하다_정시인_경우_예약가능():
     pass
-
 
 def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생():
     pass
